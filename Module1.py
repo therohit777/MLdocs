2efdcbf3bf2041b39710662e9e0464f4eca46a43
Rohit Pandey <rohitpandeyofficial777@gmail.com>
2025-01-02 13:34:05 +0530

    Feat: changes

---
+++ Module1.py
@@ -0,0 +1,111 @@
+# Notes:
+# Machine Learning:
+# A field in. Computer Science that uses statistical techniques to give computer the ability to learn with data without being explicitly programmed.
+
+# Explicit Programming:
+#    1. Data + Program -> output
+# Machine Learning:
+#    1. Data + output -> Program
+
+
+
+
+
+# History:
+# Started with symbolic AI:
+# Symbolic AI: If-Else based big knowledge system.
+# Expert System: with help of Expert people of a field Symbolic based knowledge systems were built.
+
+# Symbolic AI: Failed in Fuzzy logic. For Ex: Dog breed recognition through Image. So, ML came into play.
+
+# Bigger domain wise: Al>ML>DL
+# Bigger power wise: DL > ML
+
+# DL: Same as ML but, more powerful and its algorithm are inspired by biology of our neurons
+
+# Difference ML Vs DL
+# 1. Features:
+#        In ML have to provided, in DL 
+#        automatically extracted.
+#   2. Efficiency: 
+#       In ML gets saturated after a 
+#       point, In DL by addding more   
+#       layers of neurons can be 
+#       increase.
+#  3. Datasets:
+#       ML works better in smaller 
+#       datasets, DL works better in 
+#       larger datasets
+
+
+
+
+
+
+
+# Types: machine Learning
+#   1. Supervised
+#   2. ⁠Unsupervised 
+#   3. ⁠Semi-Supervised 
+#   4. ⁠Reinforcement 
+
+# 1. Supervised:
+#        Both Input and output 
+#        datasets available 
+       
+#        Types:
+#        A. Regression 
+#        B. Classification 
+       
+#        Regression: numerical data as 
+#        output(numerical value as 
+#        output)
+      
+#       Classification: categorical data 
+#       as output.(Strjngs, characters 
+#       as data)
+
+
+#    2. Unsupervised: 
+#        Only input are present as 
+#        datasets.
+        
+#        Types:
+#         A. Clustering:
+#         B. Dimensionality Reduction:
+#         C. Anomaly Detection:
+#         D. Association Rule:
+
+#       A. Clustering: Grouping of 
+#           similar datasets
+     
+#       B. Dimensionality Reduction: 
+#           Reducing the no. Of columns 
+#           in datasets by combing 
+#           similar related columns
+
+#      C. Anomaly Detection: 
+#          Removing of unknown data      
+#          from datasets which wasn’t 
+#          present while training 
+#          through datasets.
+
+#      D. Association Rule: fetching 
+#          insights from associated 
+#          data(data mining) and 
+#          drawing conclusions
+
+#    3. Semi - Supervised:
+#         We label or provide output of      
+#         few datasets manually the    
+#         rest data gets labelled by 
+#         machine learning algorithms 
+#         itself.
+  
+#   4. Reinforcement Learning:
+#       Has no datasets: It has agents 
+#       which performs action in 
+#       environment, gets some 
+#       feedback and basically on that 
+#       it generates own datasets and 
+#       acts positively next time
